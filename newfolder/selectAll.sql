4c7451bea0ea0e5a046fb1043d97ad83f510c1b4
ohad10799 <169071368+ohad10799@users.noreply.github.com>
2025-05-06 14:16:00 +0300

    Delete newfolder directory

--- newfolder/selectAll.sql
+++
@@ -1,7 +0,0 @@
--- שליפת כל הנתונים מכל אחת מהטבלאות
-SELECT * FROM check_in_out;
-SELECT * FROM feedback;
-SELECT * FROM guest;
-SELECT * FROM incidentreport;
-SELECT * FROM reservation;
-SELECT * FROM subscription;
